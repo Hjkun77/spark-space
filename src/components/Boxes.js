c928da78ed643d302b31038149fe32305c479724
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-11-14 02:37:31 +0800

    Fix

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Icon from './../assets/icon.png';
 import './style.css';
 
 const Boxes = (props) => {
   return(
     <div className="boxes">
-      <img src={Icon} alt="Icon" width="100"/>
+      <img src={props.image} alt="Icon" width="100"/>
       <h4>{props.title}</h4>
       <div className="box-container">
         <p>{props.paragraph}</p>
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Navbar from './Navbar';
 import Boxes from './Boxes';
 import Space from './../assets/imagine.svg';
+import Icon from './../assets/icon.png';
+import Icon1 from './../assets/icon1.png';
+import Icon2 from './../assets/icon2.png';
 import './style.css';
 
 
@@ -21,9 +24,9 @@ const LandingPage = () =>  {
         <div className="space-boxes">
           <p>How it Works</p>
           <div className="space-grid">
-            <div className="box1"><Boxes title="Open App" paragraph="You don't have to waste your precious time in finding a parking space. You just need to open the platform in any device through a mobile app or a website. What a steal! Only one click away"/></div>
-            <div className="box2"><Boxes title="Plan Parking" paragraph="You have the autonomy and control on which parking you want to reserve or see. View real-time how many people are looking currently for the space or reserve it immediately."/></div>
-            <div className="box3"><Boxes title="Find Your Space" paragraph="The platform or app allows users to show which parking space are available and who are interested on a space or who reserved it already real-time."/></div>
+            <div className="box1"><Boxes image={Icon} title="Open App" paragraph="You don't have to waste your precious time in finding a parking space. You just need to open the platform in any device through a mobile app or a website. What a steal! Only one click away"/></div>
+            <div className="box2"><Boxes image={Icon1} title="Plan Parking" paragraph="You have the autonomy and control on which parking you want to reserve or see. View real-time how many people are looking currently for the space or reserve it immediately."/></div>
+            <div className="box3"><Boxes image={Icon2} title="Find Your Space" paragraph="The platform or app allows users to show which parking space are available and who are interested on a space or who reserved it already real-time."/></div>
           </div>
         </div>
       </div>
