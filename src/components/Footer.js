7ea39faa5b0b0700956e8d54a06551a319307a15
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-11-14 10:48:31 +0800

    Footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,15 @@ import React from 'react';
 import './style.css';
 
 const Footer = () => {
-    return ( <
-        footer >
+    return ( < footer >
         <
         div className = "footer-buttons" >
         <
-        button className = "footer-button1" > < a href = "https://www.facebook.com/ateneoaeces/posts/1945747705512572
+        button className = "footer-button1" > < a href = "https://www.facebook.com/ateneoaeces/posts/1945747705512572"
         target = "_blank" > Vote Now < /a></button >
         <
-        button className = "footer-button2" > Download App < /button> <
-        /div> <
+        button className = "footer-button2" > Download App < /button> < /
+        div > <
         p className = "footer-notes" >
         Space is a utility application that was developed
         for AECES Sparks Summit in the Ateneo de Manila University. <
@@ -19,8 +18,7 @@ const Footer = () => {
         <
         br / >
         All rights reserved to Harvey Sison, Tomas Falgui III and Camille Puentespina.©2018 <
-        /p> <
-        /footer>
+        /p> < /footer >
     );
 }
 
