c4b62775a094596c6d1de926d692d01202c03997
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2018-11-14 10:37:24 +0800

    LInk update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import './style.css';
 
-const Footer = () =>  {
-  return(
-    <footer>
-      <div className="footer-buttons">
-        <button className="footer-button1"><a href="https://admu.uxsociety.org/uxnchill#home" target="_blank">Vote Now</a></button>
-        <button className="footer-button2">Download App</button>
-      </div>
-      <p className="footer-notes">
-      Space is a utility application that was developed for AECES Sparks Summit in the Ateneo de Manila University.
-      <br/>
-      <br/>
-      All rights reserved to Harvey Sison, Tomas Falgui III and Camille Puentespina. © 2018
-      </p>
-    </footer>
-  );
+const Footer = () => {
+    return ( <
+        footer >
+        <
+        div className = "footer-buttons" >
+        <
+        button className = "footer-button1" > < a href = "https://www.facebook.com/ateneoaeces/posts/1945747705512572
+        target = "_blank" > Vote Now < /a></button >
+        <
+        button className = "footer-button2" > Download App < /button> <
+        /div> <
+        p className = "footer-notes" >
+        Space is a utility application that was developed
+        for AECES Sparks Summit in the Ateneo de Manila University. <
+        br / >
+        <
+        br / >
+        All rights reserved to Harvey Sison, Tomas Falgui III and Camille Puentespina.©2018 <
+        /p> <
+        /footer>
+    );
 }
 
 export default Footer;
